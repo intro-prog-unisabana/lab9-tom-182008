089675d53c58acf7c5634919838cb99bb03a5523
Tomás Rodriguez <tomasrour@unisabana.edu.co>
2026-05-04 06:47:46 -0500

    song.py

--- song.py
+++ song.py
@@ -1,1 +1,16 @@
 # Write your code here!
+class Song:
+    def __init__(self, name, artist, length):
+        """
+        Inicializa los atributos de la canción.
+        """
+        self.name = name
+        self.artist = artist
+        self.length = length
+
+    def get_length_in_seconds(self):
+        """
+        Convierte la duración de minutos (float) a segundos.
+        1 minuto = 60 segundos.
+        """
+        return self.length * 60
