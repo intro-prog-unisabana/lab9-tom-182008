e228823ba627ac9a0e52d840681d6fae3cac260e
Tomás Rodriguez <tomasrour@unisabana.edu.co>
2026-05-04 07:13:35 -0500

    aircraft_altitude.py

--- aircraft_altitude.py
+++ aircraft_altitude.py
@@ -1,34 +1,39 @@
-
 from aircraft import Aircraft
 
 def main():
-
     model = input("Enter aircraft model: ")
     my_aircraft = Aircraft(model)
 
     while True:
         user_input = input("Enter command (A for ascent, D for descent, X to exit): ")
         
+        # .split() separa por espacios
         parts = user_input.split()
-        command = parts[0].upper() 
-        if command == "X":
+        
+       
+        if not parts:
+            continue
             
+        command = parts[0].upper()
+
+        if command == "X":
             break
         
-        elif command == "A" or command == "D":
-            
-            feet = int(parts[1])
-            
-            if command == "A":
-                
-                my_aircraft.ascend(feet)
-            else:
-                
-                my_aircraft.descend(feet)
+    
+        elif (command == "A" or command == "D") and len(parts) > 1:
+            try:
+                feet = int(parts[1])
+                if command == "A":
+                    my_aircraft.ascend(feet)
+                else:
+                    my_aircraft.descend(feet)
+            except ValueError:
+                print("Invalid number of feet.")
         
         else:
-            print("Invalid command.")
+            print("Invalid command or missing parameters.")
 
+    
     print(f"Final altitude: {my_aircraft.altitude} feet")
 
 if __name__ == "__main__":
